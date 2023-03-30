2eea27a2ae9feae8b3e3278f3c6477f4684a0086
aniketch07 <caniket1307@gmail.com>
2023-03-30 21:18:57 +0530

    first commit

---
+++ scripts/finalDeploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+async function main() {
+  const chai = await hre.ethers.getContractFactory("chai");
+  const contract = await chai.deploy(); //instance of contract
+  await contract.deployed();
+
+  console.log("address of contract:", contract.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
